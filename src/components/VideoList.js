501723c3e7b6d3d2c3df6f1a715c3c0fc7a0ef5a
github username <juliannne@gmail.com>
2018-06-12 18:05:07 -0400

    Import VideoListItem, add props

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import VideoListItem from './VideoListItem';
 
-const VideoList = () => {
+const VideoList = (props) => {
+  props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+  
   return (
     <ul className="col-md-4 list-group">
     </ul>
