724bb23455cbcd9921710b091aa8981101ea4bd3
github username <juliannne@gmail.com>
2018-06-25 10:58:00 -0400

    Add bootstrap and iframe

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+  return (
+    <div className="col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item"></iframe>
+      </div>
+    </div>
+  )
+};
+
+export default VideoDetail;
