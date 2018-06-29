00cea87526ff0f0d73286da215f7f44fdfc40dde
github username <juliannne@gmail.com>
2018-06-29 11:32:03 -0400

    Add lodash

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./containers/SearchBar";
 import YTSearch from 'youtube-api-search';
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
+import _ from 'lodash';
 import './App.css';
 
 class App extends Component {
@@ -25,11 +26,15 @@ class App extends Component {
   }
 
   render() {
+    // returns a new funciton that can only be called once every 300 ms
+    // this helps throttle user imput to make sure they don't search too often
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">VideoPlayer</h1>
-          <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+          <SearchBar onSearchTermChange={videoSearch}/>
         </header>
         <VideoDetail video={this.state.videos[0]}/>
         <VideoList
